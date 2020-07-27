cc0e2cca975b101d2f502d8e24a5a659f677c41d
niszet <niszet0016@gmail.com>
2020-07-27 09:19:42 +0900

    add check support functions

--- R/color_util.R
+++ R/color_util.R
@@ -1,19 +1,46 @@
-function(){
-shd_val_list <-  c("clear", "solid", "pct5", "pct10", "pct12", "pct15", "pct20", "pct25", "pct30", "pct35", "pct37", "pct40", "pct45", "pct50", "pct55", "pct60", "pct62", "pct65", "pct70", "pct75", "pct80", "pct85", "pct87", "pct90", "pct95", "horzStripe", "vertStripe", "reverseDiagStripe", "diagStripe", "diagCross", "diagCross", "thinHorzStripe", "thinVertStripe", "thinReverseDiagStripe", "thinDiagStripe", "thinHorzCross", "thinDiagCross")
 
-theme_color_name <- c("background1", "text1", "background2", "text2", "accent", "accent2", "accent3", "accent4", "accent5", "accent6")
+is_theme_color <- function(color){
+  # TODO NA is needed?
+  theme_color_name <- c("background1", "text1", "background2", "text2", "accent", "accent2", "accent3", "accent4", "accent5", "accent6")
+  color %in% theme_color_name
+}
+
+is_shd_val <- function(shd){
+  # TODO NA is needed?
+  shd_val_list <- c("clear", "solid", "pct5", "pct10", "pct12", "pct15", "pct20", "pct25", "pct30", "pct35", "pct37", "pct40", "pct45", "pct50", "pct55", "pct60", "pct62", "pct65", "pct70", "pct75", "pct80", "pct85", "pct87", "pct90", "pct95", "horzStripe", "vertStripe", "reverseDiagStripe", "diagStripe", "diagCross", "diagCross", "thinHorzStripe", "thinVertStripe", "thinReverseDiagStripe", "thinDiagStripe", "thinHorzCross", "thinDiagCross")
+  shd %in% shd_val_list
+}
+
+is_hex <- function(color_code){
+  stringr::str_detect(color_code, "^[0-9AaBbCcDdEeFf]{6}$")
+}
+
+is_em_val <- function(em){
+  em_val <- c("dot", NA)
+  em %in% em_val
+}
+
+is_u_val <- function(u){
+  # TODO add other options
+  u_val <- c("double", NA)
+  u %in% u_val
+}
+
+fontsize_to_pt <- function(sz){
+  as.character(as.numeric(sz) / 2)
+}
+
+pt_to_fontsize <- function(pt){
+  as.character(as.numeric(pt) * 2)
+}
+
+
+function(){
 
 # theme_tint <- c(NA, "33", "66", "99", NA, NA)
 
 # theme_shade <- c(NA, NA, NA, NA, "BF", "80")
 
-em_val <- c("dot")
-
-u_val <- c("double")
-
-is_hex <- function(color_code){
-  stringr::str_detect(color_code, "^[0-9AaBbCcDdEeFf]{6}$")
-}
 
 # color_code_to_rgb
 # function(){
@@ -22,13 +49,7 @@ is_hex <- function(color_code){
 #
 
 # sz value in ooxml is x2 times of pt in Word.
-fontsize_to_pt <- function(sz){
-  as.character(as.numeric(sz) / 2)
-}
 
-pt_to_fontsize <- function(pt){
-  as.character(as.numeric(pt) * 2)
-}
 
 
 misc <- c("center", "both", "around", "text", "dashSmallGap", "auto")
