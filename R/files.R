6bcca195e575a3bd77bf5ccb3619956c0cf574d1
niszet <niszet0016@gmail.com>
2020-08-10 11:30:19 +0900

    rename arguments related to files

--- R/files.R
+++ R/files.R
@@ -27,8 +27,8 @@ read_styles <- function(file){
 #' Write docx file with original reference.docx and updated style xml
 #'
 #' @param styles_xml `xml_node` object with `styles`
-#' @param org_docx reference docx file name as a `character`. Use files except `word/styles.xml`.
-#' @param new_docx new docx file name as a `character`.
+#' @param ref_file reference docx file name as a `character`. Use files except `word/styles.xml`.
+#' @param new_file new docx file name as a `character`.
 #'
 #' @return None
 #' @export
@@ -38,7 +38,7 @@ read_styles <- function(file){
 #'   write_styles(xml, "reference.docx", "updated.docx")
 #' }
 #'
-write_styles <- function(styles_xml, org_docx, new_docx){
+write_styles <- function(styles_xml, ref_file, new_file){
 
   if(!all(
     any(class(styles_xml) %in% c("docx_styles")),
@@ -48,13 +48,13 @@ write_styles <- function(styles_xml, org_docx, new_docx){
   }
 
   tmpdir = fs::file_temp()
-  officer::unpack_folder(file = org_docx, folder = tmpdir)
+  officer::unpack_folder(file = ref_file, folder = tmpdir)
 
   xml2::write_xml(styles_xml,
                   fs::path_abs(fs::path(tmpdir, "word/styles.xml")),
                   format="format_whitespace")
 
-  officer::pack_folder(new_docx, folder = tmpdir)
+  officer::pack_folder(new_file, folder = tmpdir)
 
   fs::dir_delete(tmpdir)
 }
